37d0a31bdaf9148d80c68c7d346031abe70fcfda
maoqijun <1535601213@qq.com>
2026-09-08 17:24:46 +0800

    Deploy explorer with v2 data

--- explorer/runtime-config.js
+++ explorer/runtime-config.js
@@ -1,4 +1,4 @@
-const DATA_ASSET_BASE = "https://pub-d3115517269f4a6ebd6e6a8143586f51.r2.dev";
+const DATA_ASSET_BASE = "https://pub-d3115517269f4a6ebd6e6a8143586f51.r2.dev/v2";
 
 window.VA_USAGE_CONFIG = {
   // Change only DATA_ASSET_BASE when the storage domain changes.

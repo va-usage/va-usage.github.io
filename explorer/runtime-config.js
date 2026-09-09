ecef8bc7d2cf0a2f9321cc9e0bacfe278a570acf
maoqijun <1535601213@qq.com>
2026-09-09 09:45:33 +0800

    Bust cache for refreshed interface images

--- explorer/runtime-config.js
+++ explorer/runtime-config.js
@@ -1,7 +1,9 @@
 const DATA_ASSET_BASE = "https://pub-d3115517269f4a6ebd6e6a8143586f51.r2.dev/v2";
+const IMAGE_ASSET_VERSION = "20260909-highres-crops-1";
 
 window.VA_USAGE_CONFIG = {
   // Change only DATA_ASSET_BASE when the storage domain changes.
   dataAssetBase: DATA_ASSET_BASE,
   appBasePath: "/explorer",
+  imageAssetVersion: IMAGE_ASSET_VERSION,
 };
